648c3300c2fdd8ec0a0aaa3b73fa6b55f45a8ec3
gauravgautam2003 <gauravgautam9865@gmail.com>
2026-03-10 23:21:17 +0530

    solve fibonacci & prime number

---
+++ problems/primeNumber.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the value of n : ";
+    cin >> n;
+    bool isPrime = true;
+
+    for(int i = 2; i < n; i++){
+        if(n%i == 0){
+            isPrime = false;
+            break;
+        }
+    }
+
+    if(isPrime == false){
+        cout << "Not a prime number :";
+    }else{
+        cout << "prime number :"; 
+    }
+}
